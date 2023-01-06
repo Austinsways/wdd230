dc4f780d9c5d0a5fd59c7674e6b9ef558baaff7c
Austinsways <eld20009@byui.edu>
2023-01-05 22:40:22 -0700

    created portal page generally

---
+++ scripts/indexScripts.js
@@ -0,0 +1,8 @@
+const currentDate = new Date();
+const currentYearString = currentDate.getFullYear();
+document.querySelector("#currentYear").textContent = currentYearString;
+
+
+document.querySelector("#lastModified").textContent = document.lastModified;
+
+
