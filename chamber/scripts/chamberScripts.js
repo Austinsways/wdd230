df8c0759228f74e046e1f30c66cc50e6c34743e9
Austinsways <eld20009@byui.edu>
2023-02-02 15:58:52 -0700

    fixed date for banner warning

--- chamber/scripts/chamberScripts.js
+++ chamber/scripts/chamberScripts.js
@@ -33,7 +33,7 @@ function myFunction() {
     
 }
 
-if (dates[currentDate.getDay()] == "Thursday" || dates[currentDate.getDay()] == "Tuesday"){
+if (dates[currentDate.getDay()] == "Monday" || dates[currentDate.getDay()] == "Tuesday"){
     var banner = document.createElement("h3");
     banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
     banner.style.textAlign = "center";
